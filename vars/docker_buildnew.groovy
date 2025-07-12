942ce628174f381398c4a573130e2f974709ffff
Ankit Gupta <32204956+Rajeshgupta123456789@users.noreply.github.com>
2025-07-12 15:47:32 -0400

    Update and rename docker_buildnew.groovy to docker_build.groovy

--- vars/docker_buildnew.groovy
+++
@@ -1,9 +0,0 @@
-def call_docker_build_debug(String ProjectName, String ImageTag, String DockerHubUser) {
-  script {
-    echo "⚠️ Using DockerHub user: ${DockerHubUser}"
-    def fullTag = "${DockerHubUser}/${ProjectName}:${ImageTag}"
-    echo "🔨 Building Docker image: ${fullTag}"
-    echo "🧪 DEBUG: docker_build.groovy latest version"
-    sh "docker build -t ${fullTag} ."
-  }
-}
