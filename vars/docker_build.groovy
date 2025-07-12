424f6d5ef38265e11620e94dc4778570a16e62ec
Ankit Gupta <32204956+Rajeshgupta123456789@users.noreply.github.com>
2025-07-12 15:31:34 -0400

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -3,6 +3,7 @@ def call(String ProjectName, String ImageTag, String DockerHubUser) {
     echo "⚠️ Using DockerHub user: ${DockerHubUser}"
     def fullTag = "${DockerHubUser}/${ProjectName}:${ImageTag}"
     echo "🔨 Building Docker image: ${fullTag}"
+    echo "🧪 DEBUG: docker_build.groovy latest version"
     sh "docker build -t ${fullTag} ."
   }
 }
