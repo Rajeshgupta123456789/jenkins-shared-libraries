53bf1b7c8709f0d2035cb19128696eb35e4b01c0
Ankit Gupta <32204956+Rajeshgupta123456789@users.noreply.github.com>
2025-07-12 14:09:36 -0400

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,7 @@
-// Define function
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+def call(String projectName, String imageTag, String dockerHubUser) {
+    script {
+        def imageFullName = "${dockerHubUser}/${projectName}:${imageTag}"
+        echo "🔥 Building image: ${imageFullName}"
+        sh "docker build -t ${imageFullName} ."
+    }
 }
