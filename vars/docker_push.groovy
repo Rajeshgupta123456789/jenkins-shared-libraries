e7f8100bf60036c5c76aa17ae44410cb9d9bb5c6
Ankit Gupta <32204956+Rajeshgupta123456789@users.noreply.github.com>
2025-07-12 14:04:45 -0400

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,5 +1,5 @@
-withCredentials([usernamePassword(credentialsId: 'docker', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-    sh """
-      echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin
-    """
+def call(String ){
+  withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
+      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass} --password-stdin"
+  }
 }
